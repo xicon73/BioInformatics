d8345bbda2d6291cfdf491f29c59ea28ecc9006e
Francisco Costa <xicon73@gmail.com>
2017-10-13 15:26:39 +0100

    aula 2

---
+++ ficheiros.py
@@ -0,0 +1,10 @@
+def readFasta(file):
+    id = ""
+    Seq = ""
+    f=open(file,"r")
+    for line in f:
+        if line.startswith(">"):
+            id=line[1:]
+        else: Seq+=line
+    f.close()
+    return(id,Seq)
